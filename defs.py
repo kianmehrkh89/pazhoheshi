cf5836c58f207074ebb092308a0fb4a841633056
Amir Kh <kianmehr.khalili.1389@gmail.com>
2024-11-06 10:46:13 +0330

    fix :separate defs

---
+++ defs.py
@@ -0,0 +1,8 @@
+def check_user(name,users):
+    if name in users:
+        return True
+    return False
+def check_book(BName,libry):
+    if BName in libry:
+        return True
+    return False
